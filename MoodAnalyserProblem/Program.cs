278843130f58ff6e53340d1088628ae40c8d1640
vundelayamini <vundelayamini@gmail.com>
2021-04-24 09:27:37 +0530

    [yamini]Add: Welcome message to mood analyser

---
+++ MoodAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser:");
+        }
+    }
+}
